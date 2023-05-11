3be5f9d74f7894282c01307a8ad83455c6dec192
An Nguyen <132466715+WwOwW4fun@users.noreply.github.com>
2023-05-11 17:38:28 +0700

    Add files via upload

---
+++ UVA 10954 Add All .cpp
@@ -0,0 +1,62 @@
+/* 
+Author : Nguyen Xuan An 
+Gmail : xuanan437@gmail.com
+*/   
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl "\n"
+#define for0(i ,l ,r) for(int i = (l); i < (r); ++i)
+#define for1(i, l, r) for(int i = (l); i >= (r); --i)
+#define for2(v) for(auto it : v)
+#define reset(a) memset(a,0,sizeof(a))
+#define mem(a) memset(a,-1,sizeof(a))
+#define membo(a) memset(a,false,sizeof(a))
+
+#define vi vector<int>
+#define vpi vector<pair<int, int>>
+#define pii pair<int,int>
+#define fi first
+#define se second
+#define prq priority_queue
+#define pb push_back
+
+#define output ios::sync_with_stdio(0);cin.tie(0);
+#define fastio 	ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL);
+#define PI 3.1415926535897932385
+#define INF 10000000
+
+#define BIT(n) (1<<(n))
+#define AND(a,b) ((a) & (b))
+#define OR(a,b) ((a) | (b))
+#define XOR(a,b) ((a) ^ (b))
+
+int main(){
+	int n;
+	while(cin >> n)
+	{
+	if(n == 0)
+	break;
+	else{
+	
+	priority_queue<int ,vector<int>, greater<int>> pq;
+	for(int i = 0; i < n ; i++)
+	{
+		int x; cin >> x;
+		pq.push(x);
+	}
+    int sum = 0;
+	while(pq.size()!=1)
+	{
+		int x= 0;
+		x += pq.top();
+		pq.pop();
+		x += pq.top();
+		pq.pop();
+		sum+=x;
+		pq.push(x);
+	}
+	cout << sum  << endl;
+	}
+}
+}
